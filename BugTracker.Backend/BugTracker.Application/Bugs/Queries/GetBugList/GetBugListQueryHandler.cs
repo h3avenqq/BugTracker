dfb906f91a667f5a1625c57500eac9db8bb94ee5
Maxim Shamrev <shmarev082@gmail.com>
2022-08-08 00:01:39 +0300

    changed .gitignore

--- BugTracker.Backend/BugTracker.Application/Bugs/Queries/GetBugList/GetBugListQueryHandler.cs
+++ BugTracker.Backend/BugTracker.Application/Bugs/Queries/GetBugList/GetBugListQueryHandler.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BugTracker.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTracker.Application.Bugs.Queries.GetBugList
 {
     public class GetBugListQueryHandler
+        : IRequestHandler<GetBugListQuery, BugListVm>
     {
+        private readonly IBugsDbContext _dbContext;
+        private readonly IMapper _mapper;
 
+        public GetBugListQueryHandler(IBugsDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<BugListVm> Handle(GetBugListQuery request, 
+            CancellationToken cancellationToken)
+        {
+            var bugsQuery =
+                await _dbContext.Bugs.AsQueryable()
+                    .ProjectTo<BugLookupDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken);
+
+            return new BugListVm { Bugs = bugsQuery };
+        }
     }
 }
